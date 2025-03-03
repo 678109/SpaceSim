bbdadf094c9780097e443830749a111f10e4f25d
Lilsaucyho <142339749+Lilsaucyho@users.noreply.github.com>
2025-03-03 17:30:57 +0100

    Task 1 ferdig

---
+++ SpaceSim/SpaceObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SpaceSimulator
+{
+    public class SpaceObject
+    {
+
+        public String Name { get; protected set;}
+
+        public SpaceObject(string name) {
+            Name = name;
+        }
+            public virtual void Draw() {
+            Console.WriteLine(Name);
+        }
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Star  : ");
+            base.Draw();
+        }
+    }
+
+    public class Planet : SpaceObject
+    {
+        public Planet(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+    }
+
+    public class Moon : SpaceObject {
+        public Moon(String name) : base(name) { }
+        public override void Draw() {
+            Console.Write("Moon  : ");
+            base.Draw();
+        }
+    }
+}
---
+++ SpaceSimConsole/Astronomy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceSimulator;
+class Astronomy
+{
+    static void Main()
+    {
+        List<SpaceObject> solarSystem = new List<SpaceObject> {
+                new Star("Sun"),
+                new Planet("Mercury"),
+                new Planet("Venus"),
+                new Planet("Terra"),
+                new Moon("The moon")
+            };
+
+        foreach (SpaceObject obj in solarSystem)
+        {
+            obj.Draw();
+        }
+        Console.ReadLine();
+    }
+
+}
